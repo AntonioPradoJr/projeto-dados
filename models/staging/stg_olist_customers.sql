18e0b2de82e16f68a81b0c567b073c3c28feb1b6
Antonio Prado <apdj.ti@gmail.com>
2026-07-13 20:19:41 +0000

    adiciona stg_olist_orders e stg_olist_customers

---
+++ models/staging/stg_olist_customers.sql
@@ -0,0 +1,12 @@
+WITH source AS(
+    SELECT * FROM {{ source('raw', 'olist_orders') }}
+)
+
+SELECT
+    customer_id,
+    customer_unique_id,
+    CAST(customer_zip_code_prefix AS INT64)                AS customer_zip_code_prefix,
+    LOWER(customer_city)                                   AS customer_city,
+    LOWER(customer_state)                                  AS customer_state
+FROM
+    source
---
+++ models/staging/stg_olist_orders.sql
@@ -0,0 +1,20 @@
+WITH source AS (
+    SELECT * FROM {{ source('raw', 'olist_orders') }}
+)
+
+SELECT  
+    order_id, 
+    order_item_id, 
+    customer_id,
+    seller_id, 
+    LOWER(TRIM(order_status))                   AS status,
+    DATE(order_purchase_timestamp)              AS data_compra,
+    DATE(order_approved_at)                     AS data_aprovacao_pagamento,
+    DATE(order_estimated_delivery_date)         AS data_estimativa_entrega,
+    DATE(order_delivered_carrier_date)          AS data_entregue_transportadora,
+    DATE(order_delivered_customer_date)         AS data_entregue_cliente
+FROM
+    source
+
+
+
