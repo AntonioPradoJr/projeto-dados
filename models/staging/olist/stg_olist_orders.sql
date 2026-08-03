2102c23ff2b7cc574c21e97f0eb3527c0ee9a806
Antonio Prado <apdj.ti@gmail.com>
2026-08-03 20:30:28 +0000

    Adicionando código Jinja para transformação de tipo dos campos de data.

--- models/staging/olist/stg_olist_orders.sql
+++ models/staging/olist/stg_olist_orders.sql
@@ -2,18 +2,23 @@ WITH source AS (
     SELECT * FROM {{ source('olist', 'olist_orders') }}
 )
 
+
+{% set date_columns = [('order_purchase_timestamp', 'data_compra'), 
+                       ('order_approved_at', 'data_aprovacao_pagamento'), 
+                       ('order_estimated_delivery_date', 'data_estimativa_entrega'), 
+                       ('order_delivered_carrier_date', 'data_entregue_transportadora'), 
+                       ('order_delivered_customer_date', 'data_entregue_cliente') 
+] %}
+
 SELECT  
     order_id, 
     order_item_id, 
     customer_id,
     seller_id, 
     LOWER(TRIM(order_status))                   AS status,
-    DATE(order_purchase_timestamp)              AS data_compra,
-    DATE(order_approved_at)                     AS data_aprovacao_pagamento,
-    DATE(order_estimated_delivery_date)         AS data_estimativa_entrega,
-    DATE(order_delivered_carrier_date)          AS data_entregue_transportadora,
-    DATE(order_delivered_customer_date)         AS data_entregue_cliente
-    
+    {% for original, novo in date_columns %}
+        DATE({{ original }}) AS {{ novo }} {% if not loop.last %},{% endif %} 
+    {% endfor %}   
 FROM
     source
 
